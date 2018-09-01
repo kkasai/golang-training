99e2c82155507dbd5ca4bd6c6d2fab613f3dfb5f
Keita Kasai <keita.kasai.1230@gmail.com>
2018-09-02 01:19:50 +0900

    5.17

---
+++ ch05/ex17/elementsByTagName.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("error")
+		return
+	}
+	resp, err := http.Get(os.Args[1])
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return
+	}
+
+	images := ElementsByTagName(doc, "img")
+	headings := ElementsByTagName(doc, "img")
+	printElement(images)
+	printElement(headings)
+}
+
+func printElement(nodes []*html.Node) {
+	for _, n := range nodes {
+		var attrs string
+		for _, a := range n.Attr {
+			attrs += fmt.Sprintf(" %s=\"%s\"", a.Key, a.Val)
+		}
+		fmt.Printf("<%s%s>\n", n.Data, attrs)
+	}
+}
+
+func ElementsByTagName(doc *html.Node, name ...string) []*html.Node {
+	var element []*html.Node
+	var f = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			for _, tag := range name {
+				if n.Data == tag {
+					element = append(element, n)
+				}
+			}
+		}
+	}
+	forEachNode(doc, f)
+	return element
+}
+
+func forEachNode(n *html.Node, pre func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre)
+	}
+}
---
+++ ch05/ex17/run.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go run elementsByTagName.go http://www.gopl.io
